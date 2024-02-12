2326eca0138adfca6ade4026db8a7517a0f6ee7e
komphet <komphet.meesab@krungthai-axa.co.th>
2024-02-12 17:34:37 +0700

    Finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,96 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState, useRef } from "react";
 
 export default function Home() {
+
+  const [sourceText,setSourceText] = useState("")
+  const [cleanText,setCleanText] = useState("")
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  
+  const focusTextarea = () => {
+    if (textareaRef.current) {
+      textareaRef.current.focus();
+    }
+  };
+
+  function removeEmptyLinesAndTrim(text: string) {
+    // Split text into lines
+    var lines = text.split('\n');
+
+    // Remove empty lines, trim whitespace from each line, and add empty line above lines containing '#'
+    var filteredLines: string[] = [];
+    lines.forEach(function(line, index) {
+        // Remove leading and trailing whitespace from each line
+        var trimmedLine = line.trim();
+        // Check if the line is not empty after trimming
+        if (trimmedLine.length > 0) {
+            if (trimmedLine.includes('#') && index !== 0) {
+                // Add an empty line above lines containing '#'
+                filteredLines.push('');
+            }
+            filteredLines.push(trimmedLine);
+        }
+    });
+
+    // // Remove the last character of each line
+    // filteredLines = filteredLines.map(function(line) {
+    //     return line.slice(0, -1); // Slice from beginning to second last character
+    // });
+
+    // Join the filtered lines back together
+    var result = filteredLines.join('\n');
+
+    return result;
+  }
+
+  const handleCopy = (cleanedText: string) => {
+    navigator.clipboard.writeText(cleanedText)
+      .then(() => {
+        console.log('Cleaned Text copied to clipboard');
+        alert('Cleaned Text copied to clipboard')
+      })
+      .catch(err => {
+        console.error('Error copying text:', err);
+      });
+  };
+
+  useEffect(()=>{
+    if(sourceText !== ""){
+
+      const cleanedText = removeEmptyLinesAndTrim(sourceText);
+      // console.log(cleanedText);
+      setCleanText(cleanedText)
+      focusTextarea();
+      handleCopy(cleanedText);
+    }
+  },[sourceText]);
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="flex w-full">
+      <textarea
+            id="sourceText"
+            name="sourceText"
+            placeholder="FK Source"
+            // rows={50}
+            className="border rounded-lg p-2 h-screen w-3/6"
+            value={sourceText}
+            // defaultValue={sourceText}
+            onChange={(e)=>setSourceText(e.target.value)}
+          />
+          <textarea
+            ref={textareaRef}
+            id="cleanText"
+            name="cleanText"
+            // rows={50}
+            placeholder="Clean ENV"
+            className="border rounded-lg p-2 h-screen w-3/6"
+            value={cleanText}
+            // defaultValue={cleanText}
+            onChange={(e)=>console.log(e.target.value)}
+          />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
